cf3c7f5dbfc887ddf70c88a8b583e34495a6c01d
geru-scotland <aingeru.scotland@gmail.com>
2025-02-09 13:44:53 +0100

    [Tests][Game][CI] Game tests init, and github actions CI test workflow

---
+++ tests/test_game.py
@@ -0,0 +1,19 @@
+import os
+from unittest.mock import patch
+from game.main import init_systems, Game
+
+# Para evitar cuando lo ponga en GH actions
+# que falle por no disponer de video mode.
+
+if os.getenv("CI"):
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+@patch.object(Game, 'run', side_effect=lambda: None)
+def test_game_runs(mock_run):
+    try:
+        init_systems()
+        game = Game()
+        game.run()
+        assert True
+    except Exception as e:
+        assert False, f"Game failed to start: {e}"
